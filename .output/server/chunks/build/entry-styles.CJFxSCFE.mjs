6247960cc9a1c1a41ad5ad44ddb70997ecd88fe4
unknown <sevalinoismail@gmail.com>
2024-07-04 15:01:37 +0700

    update

--- .output/server/chunks/build/entry-styles.CJFxSCFE.mjs
+++
@@ -1,6 +0,0 @@
-const main = "body{box-sizing:border-box;font-family:Arial,sans-serif;margin:0;padding:0}.topnav{background-color:#333;overflow:hidden}.topnav a{color:#f2f2f2;float:left;font-size:17px;padding:14px 16px;text-align:center;text-decoration:none}.topnav a:hover{background-color:#ddd;color:#000}.topnav a.active{background-color:#04aa6d;color:#fff}.hero{background-color:#083316}";
-
-const entryStyles_CJFxSCFE = [main];
-
-export { entryStyles_CJFxSCFE as default };
-//# sourceMappingURL=entry-styles.CJFxSCFE.mjs.map
